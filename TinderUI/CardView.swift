20b3eaa754ab6f408912f3d9b0cc624baf124a68
konojunya <junya03dance@gmail.com>
2017-07-04 14:19:04 +0900

    TinderUI完成

---
+++ TinderUI/CardView.swift
@@ -0,0 +1,53 @@
+//
+//  CardView.swift
+//  TinderUI
+//
+//  Created by konojunya on 2017/07/04.
+//  Copyright © 2017年 konojunya. All rights reserved.
+//
+
+import UIKit
+
+class CardView: UIView {
+    
+    let imageView = UIImageView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.setUp()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setUp(){
+        
+        // shadow
+        self.layer.shadowColor = UIColor.black.cgColor
+        self.layer.shadowOpacity = 0.25
+        self.layer.shadowOffset = CGSize(width: 0, height: 1.5)
+        self.layer.shadowRadius = 4.0
+        self.layer.shouldRasterize = true
+        self.layer.rasterizationScale = UIScreen.main.scale
+        
+        //corner radius
+        self.layer.cornerRadius = 10.0
+        self.clipsToBounds = true
+        
+        let contentView = UIView(frame: self.bounds)
+        self.addSubview(contentView)
+        
+        contentView.addSubview(self.imageView)
+        self.imageView.snp.makeConstraints { make in
+            make.size.equalTo(contentView)
+            make.center.equalTo(contentView)
+        }
+        
+    }
+    
+    func setImage(image: UIImage) {
+        self.imageView.image = image
+    }
+    
+}
--- TinderUI/ViewController.swift
+++ TinderUI/ViewController.swift
@@ -12,8 +12,76 @@ import ZLSwipeableViewSwift
 
 class ViewController: UIViewController {
     
+    public var numberOfActiveView = UInt(4)
+    public var nextView: (() -> UIView?)?
+    public var previousView: (() -> UIView?)?
+    
     let swipeableView = ZLSwipeableView()
-    let image = []
+    let images = [
+        UIImage(named: "hasikan1"),UIImage(named: "hasikan2"),UIImage(named: "hasikan3"),
+        UIImage(named: "hasikan4"),UIImage(named: "hasikan5"),UIImage(named: "hasikan6")
+    ]
+    var imageIndex = 0
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        self.swipeableView.nextView = {
+            return self.nextCardView()
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = UIColor.white
+        self.view.addSubview(self.swipeableView)
+        self.swipeableView.snp.makeConstraints { make in
+            make.left.equalTo(self.view).offset(50)
+            make.right.equalTo(self.view).offset(-50)
+            make.top.equalTo(self.view).offset(120)
+            make.bottom.equalTo(self.view).offset(-100)
+        }
+        self.swipeableView.didStart = {view, location in
+            print("Did start swiping view at location: \(location)")
+        }
+        self.swipeableView.swiping = {view, location, translation in
+            print("Swiping at view location: \(location) translation: \(translation)")
+        }
+        self.swipeableView.didEnd = {view, location in
+            print("Did end swiping view at location: \(location)")
+        }
+        self.swipeableView.didSwipe = {view, direction, vector in
+            print("Did swipe view in direction: \(direction), vector: \(vector)")
+        }
+        self.swipeableView.didCancel = {view in
+            print("Did cancel swiping view")
+        }
+        self.swipeableView.didTap = {view, location in
+            print("Did tap at location \(location)")
+        }
+        self.swipeableView.didDisappear = { view in
+            print("Did disappear swiping view")
+        }
+        
+    }
+
+    func nextCardView() -> CardView? {
+        if self.imageIndex >= self.images.count {
+            self.imageIndex = 0
+        }
+        
+        let cardView = CardView(frame: self.swipeableView.bounds)
+        guard let image = self.images[self.imageIndex] else { return cardView }
+        cardView.setImage(image: image)
+        self.imageIndex += 1
+        
+        return cardView
+        
+    }
     
 }
 
